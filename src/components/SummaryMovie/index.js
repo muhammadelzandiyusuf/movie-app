04037c6e47a9880e4bef594a8857573a8847e1c0
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2024-02-02 15:37:33 +0700

    feat: add animation detail

--- src/components/SummaryMovie/index.js
+++ src/components/SummaryMovie/index.js
@@ -1,28 +1,33 @@
-import 'assets/scss/summary.scss';
 import { summaryThumbnailImage, thumbnailImage } from 'libraries/image';
 import { IoPlayCircleOutline } from 'react-icons/io5';
 import { Fade } from 'react-awesome-reveal';
 import ListSummary from './ListSummary';
+import { Link } from 'react-router-dom';
+import urls from 'utils/urls';
+
+import 'assets/scss/summary.scss';
 
 const SummaryMovie = () => {
   return (
     <div className='summary'>
       <Fade direction='up' triggerOnce>
-        <div className='summary__left'>
-          <div className='summary__left__content'>
-            <img src={summaryThumbnailImage.image} alt={summaryThumbnailImage.alt} />
-            <div className='summary__left__overlay' />
-            <div className='summary__left__wrapper'>
-              <div className='summary__left__cover'>
-                <img src={thumbnailImage.image} alt={thumbnailImage.alt} />
-              </div>
-              <div className='summary__left__text'>
-                <IoPlayCircleOutline size={108} />
-                <div>The Ministry of Ungentlemanly Warfare</div>
+        <Link to={`${urls.movie}/url`}>
+          <div className='summary__left'>
+            <div className='summary__left__content'>
+              <img src={summaryThumbnailImage.image} alt={summaryThumbnailImage.alt} />
+              <div className='summary__left__overlay' />
+              <div className='summary__left__wrapper'>
+                <div className='summary__left__cover'>
+                  <img src={thumbnailImage.image} alt={thumbnailImage.alt} />
+                </div>
+                <div className='summary__left__text'>
+                  <IoPlayCircleOutline size={108} />
+                  <div>The Ministry of Ungentlemanly Warfare</div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </Link>
       </Fade>
       <div className='summary__right'>
         <ListSummary />
--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -1,10 +1,11 @@
 import { FaPlus, FaStar } from 'react-icons/fa';
 import Youtube from 'react-youtube';
+import { Fade } from 'react-awesome-reveal';
 
-import 'assets/scss/movieDetail.scss';
 import { useEffect, useRef } from 'react';
 import Button from 'components/Button';
 import { thumbnailImage } from 'libraries/image';
+import 'assets/scss/movieDetail.scss';
 
 const frameRatio = [1180, 640];
 
@@ -30,17 +31,19 @@ const MovieDetail = () => {
 
   return (
     <div className='movie__detail'>
-      <div className='movie__detail__header'>
-        <h1 className='font__size--36 font__weight--700'>Indoneisa</h1>
-        <div className='movie__detail__header__rate'>
-          <span className='font__size--16'>Rating</span>
-          <div className='movie__detail__header__number'>
-            <FaStar size={28} />
-            <span className='font__size--24 font__weight--700 margin__left--8px'>7.8</span>
-            <span className='font__size--24 color__grey'>/10</span>
+      <Fade direction='up' triggerOnce cascade>
+        <div className='movie__detail__header'>
+          <h1 className='font__size--36 font__weight--700'>Indoneisa</h1>
+          <div className='movie__detail__header__rate'>
+            <span className='font__size--16'>Rating</span>
+            <div className='movie__detail__header__number'>
+              <FaStar size={28} />
+              <span className='font__size--24 font__weight--700 margin__left--8px'>7.8</span>
+              <span className='font__size--24 color__grey'>/10</span>
+            </div>
           </div>
         </div>
-      </div>
+      </Fade>
       <div ref={frameEl} className='movie__detail__video'>
         <Youtube
           videoId='fXqQ2tYW5Sk'
@@ -56,50 +59,52 @@ const MovieDetail = () => {
           }}
         />
       </div>
-      <div className='movie__detail__content'>
-        <div className='movie__detail__content__description'>
-          <div className='movie__detail__genre'>
-            <div className='movie__detail__genre__item'>
-              <span className='font__size--14'>Romance</span>
+      <Fade direction='up' triggerOnce cascade>
+        <div className='movie__detail__content'>
+          <div className='movie__detail__content__description'>
+            <div className='movie__detail__genre'>
+              <div className='movie__detail__genre__item'>
+                <span className='font__size--14'>Romance</span>
+              </div>
+              <div className='movie__detail__genre__item'>
+                <span className='font__size--14'>Komedi</span>
+              </div>
             </div>
-            <div className='movie__detail__genre__item'>
-              <span className='font__size--14'>Komedi</span>
+            <div className='movie__detail__content__description__wrapper'>
+              <div className='movie__detail__content__description__image'>
+                <img src={thumbnailImage.image} alt={thumbnailImage.alt} />
+              </div>
+              <div className='movie__detail__content__wrapper'>
+                <div className='font__size--16 margin__bottom--16px'>Description</div>
+                <div className='font__size--16'>
+                  Director{' '}
+                  <span className='color__primary font__weight--700 margin__left--8px'>
+                    David Fincer
+                  </span>
+                </div>
+                <div className='font__size--16'>
+                  Writers{' '}
+                  <span className='color__primary font__weight--700 margin__left--8px'>
+                    Chuck Palahniuk Jim Uhls
+                  </span>
+                </div>
+                <div className='font__size--16'>
+                  Stars{' '}
+                  <span className='color__primary font__weight--700 margin__left--8px'>
+                    David Fincer
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className='movie__detail__content__description__wrapper'>
-            <div className='movie__detail__content__description__image'>
-              <img src={thumbnailImage.image} alt={thumbnailImage.alt} />
-            </div>
-            <div className='movie__detail__content__wrapper'>
-              <div className='font__size--16 margin__bottom--16px'>Description</div>
-              <div className='font__size--16'>
-                Director{' '}
-                <span className='color__primary font__weight--700 margin__left--8px'>
-                  David Fincer
-                </span>
-              </div>
-              <div className='font__size--16'>
-                Writers{' '}
-                <span className='color__primary font__weight--700 margin__left--8px'>
-                  Chuck Palahniuk Jim Uhls
-                </span>
-              </div>
-              <div className='font__size--16'>
-                Stars{' '}
-                <span className='color__primary font__weight--700 margin__left--8px'>
-                  David Fincer
-                </span>
-              </div>
-            </div>
+          <div className='movie__detail__content__watch'>
+            <Button type='primary' fullWidth>
+              <FaPlus size={18} />
+              <span className='font__size--20 font__weight--700'>Watchlist</span>
+            </Button>
           </div>
         </div>
-        <div className='movie__detail__content__watch'>
-          <Button type='primary' fullWidth>
-            <FaPlus size={18} />
-            <span className='font__size--20 font__weight--700'>Watchlist</span>
-          </Button>
-        </div>
-      </div>
+      </Fade>
     </div>
   );
 };
