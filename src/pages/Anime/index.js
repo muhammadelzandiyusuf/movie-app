29e652ef54918ca97927f4429a113735d30c4caf
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2024-02-02 21:36:43 +0700

    feat: add integrate anime page

--- src/pages/Anime/index.js
+++ src/pages/Anime/index.js
@@ -1,29 +1,69 @@
 import { Fade } from 'react-awesome-reveal';
 import CardItem from 'components/Card/CardItem';
 import Pagination from 'components/Pagination';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMovie, movieSelector } from 'stores';
+import { Fragment, useEffect, useState } from 'react';
+import Localbase from 'localbase';
+import { dbName, getSliceData, tbName } from 'utils';
+import { getMovies } from 'services';
+import { useLocation } from 'react-router-dom';
+import Loading from 'components/Loading';
 
 import 'assets/scss/movie.scss';
 import 'assets/scss/card.scss';
 
 const Anime = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const movies = useSelector(movieSelector);
+
+  const limit = 10;
+  const params = new URLSearchParams(location.search);
+  const getSlice = getSliceData(params, limit);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const localDb = new Localbase(dbName);
+    const getDataMovie = async () => {
+      const payload = { url: '/assets/data/movies.json' };
+      await getMovies(payload);
+    };
+
+    localDb
+      .collection(tbName)
+      .get()
+      .then((collections) => {
+        if (collections.length === 0 ? getDataMovie() : dispatch(getMovie(collections[0]['data'])));
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className='movie'>
+      {isLoading && <Loading />}
       <Fade direction='up' triggerOnce cascade>
         <h1 className='font__size--36 font__weight--700 margin__bottom--16px'>
           What to Watch - Anime
         </h1>
       </Fade>
       <Fade direction='up' triggerOnce cascade>
-        <div className='movie__wrapper'>
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-        </div>
+        {!isLoading && movies?.animeList?.length > 0 ? (
+          <Fragment>
+            <div className='movie__wrapper'>
+              {movies?.animeList?.slice(getSlice.from, getSlice.to)?.map((anime, index) => (
+                <CardItem {...anime} key={index} />
+              ))}
+            </div>
+            <Pagination total={movies?.animeList?.length} limit={limit} current={getSlice.page} />
+          </Fragment>
+        ) : (
+          <div className='alert margin__left--24px margin__right--24px'>
+            <h5 className='color__grey font__weight--500'>Data yang dicari tidak ditemukan</h5>
+          </div>
+        )}
       </Fade>
-      <Pagination total={20} limit={10} current={1} />
     </div>
   );
 };
